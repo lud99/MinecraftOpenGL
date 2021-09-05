c26a9a804d4c063d154730409cebe144f2bccd8e
Ludvig Magnusson <ludvig.magnusson04@gmail.com>
2021-09-05 21:09:44 +0200

    Added a .bat script to create the symlinked "common" folders in the sources

---
+++ makecommon.bat
@@ -0,0 +1,11 @@
+cd MinecraftOpenGL/Source
+mkdir Common
+cd..
+cd..
+mklink /D "./MinecraftOpenGL/Source/Common/Common" "D:\Spel\Programming\C++\MinecraftOpenGL\Common"
+
+cd Server/Source
+mkdir Common
+cd..
+cd..
+mklink /D "./Server/Source/Common/Common" "D:\Spel\Programming\C++\MinecraftOpenGL\Common"
